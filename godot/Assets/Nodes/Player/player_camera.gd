6a9db090e72aaba09a2767928adbe21408b0309e
Tomas <tomasoares@gmail.com>
2025-10-23 12:13:42 +0100

    Removed duplicated code in camera_controller script

--- godot/Assets/Nodes/Player/player_camera.gd
+++ godot/Assets/Nodes/Player/player_camera.gd
@@ -1,62 +1,7 @@
 class_name CameraController
 extends Node
-
-@export var camera_controller : Node3D
-@export var spring_arm: SpringArm3D
-@export var player : CharacterBody3D
-@export var armature : Node3D
-
-var weight = 0.05
-var rotation_set := false
-var continuous_rotation : float
-
-func init() -> void:
-	pass
 	
 func handle_camera(delta: float) -> void:
 	$AutomaticCamera.handle_camera(delta)
 	$ManualCamera.handle_camera(delta)
 	$FollowPlayer.follow()
-	
-func initialize_continuous_rotation() -> void:
-	if !rotation_set:
-		rotation_set = true
-		continuous_rotation = camera_controller.global_rotation.y
-		
-func stop_automatic_camera() -> void:
-	continuous_rotation = camera_controller.global_rotation.y
-
-func no_camera_input() -> bool:
-	var input_dir := Input.get_vector("cam_left", "cam_right", "cam_up", "cam_down")
-	var no_movement := Vector2.ZERO
-	if input_dir == no_movement:
-		weight = 0.01
-		return true
-		
-	return false
-	
-func move_automatic_camera() -> void:
-	var target_rotation := adjust_to_player_movement()
-	weight = lerp(weight, 0.1, 0.01)
-	var current_angle = camera_controller.global_rotation.y
-	camera_controller.global_rotation.y = lerp_angle(current_angle, continuous_rotation, weight)
-	
-
-func adjust_to_player_movement() -> float:
-	var player_velocity_xz = Vector3(player.velocity.x, 0, player.velocity.z)
-	var current_speed = player_velocity_xz.length()
-	
-	if current_speed < 8.0:
-		return continuous_rotation
-		
-	#get the angle of where the character is running
-	#add 90 degrees to adjust to camera angle reference
-	continuous_rotation = atan2(-player_velocity_xz.x, -player_velocity_xz.z)
-	return continuous_rotation
-
-func _input(event: InputEvent):
-	if event is InputEventMouseMotion:
-		continuous_rotation = camera_controller.global_rotation.y
-		
-	if event.is_action_pressed("cam_adjust") or event.is_action_pressed("cam_adjust_key"):
-		continuous_rotation = camera_controller.global_rotation.y
