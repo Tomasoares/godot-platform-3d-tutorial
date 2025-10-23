8fa1890fb9bf4d9105d8ab5e493340ff27f2ddee
Tomas <tomasoares@gmail.com>
2025-10-23 12:11:22 +0100

    Implemented better automatic camera

--- godot/Assets/Nodes/Player/automatic_camera.gd
+++ godot/Assets/Nodes/Player/automatic_camera.gd
@@ -5,55 +5,52 @@ extends Node3D
 
 var weight = 0.05
 var rotation_set := false
-var continuous_rotation : float
+var use_automatic_camera = false
 	
 func handle_camera(delta: float) -> void:
-	initialize_continuous_rotation()
+	should_enable_automatic_camera()
 	
-	if no_camera_input():
-		move_automatic_camera()
-	else:
-		stop_automatic_camera()
+	if !use_automatic_camera:
+		return
 	
-func initialize_continuous_rotation() -> void:
-	if !rotation_set:
-		rotation_set = true
-		continuous_rotation = camera_controller.global_rotation.y
+	move_automatic_camera()
 		
-func stop_automatic_camera() -> void:
-	continuous_rotation = camera_controller.global_rotation.y
+func should_enable_automatic_camera() -> void:
+	if player_made_camera_input():
+		use_automatic_camera = false
+		return
+		
+	if use_automatic_camera:
+		return
+		
+	var player_velocity_xz = Vector3(player.velocity.x, 0, player.velocity.z)
+	var current_speed = player_velocity_xz.length()
+	print(player.velocity.length())
+	use_automatic_camera = current_speed > 8.0
 
-func no_camera_input() -> bool:
+func player_made_camera_input() -> bool:
 	var input_dir := Input.get_vector("cam_left", "cam_right", "cam_up", "cam_down")
 	var no_movement := Vector2.ZERO
 	if input_dir == no_movement:
 		weight = 0.01
-		return true
+		return false
 		
-	return false
+	return true
 	
 func move_automatic_camera() -> void:
-	var target_rotation := adjust_to_player_movement()
+	var target_rotation := calculate_continuous_rotation()
 	weight = lerp(weight, 0.1, 0.01)
 	var current_angle = camera_controller.global_rotation.y
-	camera_controller.global_rotation.y = lerp_angle(current_angle, continuous_rotation, weight)
-	
+	camera_controller.global_rotation.y = lerp_angle(current_angle, target_rotation, weight)
 
-func adjust_to_player_movement() -> float:
+func calculate_continuous_rotation() -> float:
 	var player_velocity_xz = Vector3(player.velocity.x, 0, player.velocity.z)
-	var current_speed = player_velocity_xz.length()
+	return atan2(-player_velocity_xz.x, -player_velocity_xz.z)
 	
-	if current_speed < 8.0:
-		return continuous_rotation
-		
-	#get the angle of where the character is running
-	#add 90 degrees to adjust to camera angle reference
-	continuous_rotation = atan2(-player_velocity_xz.x, -player_velocity_xz.z)
-	return continuous_rotation
 
 func _input(event: InputEvent):
 	if event is InputEventMouseMotion:
-		continuous_rotation = camera_controller.global_rotation.y
+		use_automatic_camera = false
 		
 	if event.is_action_pressed("cam_adjust") or event.is_action_pressed("cam_adjust_key"):
-		continuous_rotation = camera_controller.global_rotation.y
+		use_automatic_camera = false
