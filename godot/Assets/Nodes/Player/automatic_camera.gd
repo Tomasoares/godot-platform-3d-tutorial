0dd719d5666b7b5aaeeee15b366be9e75f418837
Tomas <tomasoares@gmail.com>
2025-10-23 20:56:53 +0100

    reduce amount of time automatic camera is activated

--- godot/Assets/Nodes/Player/automatic_camera.gd
+++ godot/Assets/Nodes/Player/automatic_camera.gd
@@ -20,12 +20,16 @@ func should_enable_automatic_camera() -> void:
 		use_automatic_camera = false
 		return
 		
+	if is_abrupt_turn():
+		use_automatic_camera = false
+		return
+	
 	if use_automatic_camera:
 		return
 		
 	var player_velocity_xz = Vector3(player.velocity.x, 0, player.velocity.z)
 	var current_speed = player_velocity_xz.length()
-	use_automatic_camera = current_speed > 8.0
+	use_automatic_camera = current_speed > 9.0
 
 func player_made_camera_input() -> bool:
 	var input_dir := Input.get_vector("cam_left", "cam_right", "cam_up", "cam_down")
@@ -35,6 +39,15 @@ func player_made_camera_input() -> bool:
 		return false
 		
 	return true
+	
+func is_abrupt_turn() -> bool:
+	var target_rotation := calculate_continuous_rotation()
+	var current_angle = camera_controller.global_rotation.y
+	var diff_angle = target_rotation - current_angle
+	
+	var wrapped_diff = fmod(diff_angle + PI, TAU) - PI
+	var absolute_diff = abs(wrapped_diff)
+	return absolute_diff > deg_to_rad(160)
 	
 func move_automatic_camera() -> void:
 	var target_rotation := calculate_continuous_rotation()
